25983a2768918f16a476fc63fbe5fba72efd686b
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-13 21:15:26 +0100

    Update GCLogic.js

--- GCLogic.js
+++ GCLogic.js
@@ -4,9 +4,11 @@ async function deleteGenesysGroup(groupId) {
     try {
         await apiInstance.deleteGroup(groupId);
         console.log("deleteGroup returned successfully.");
+        return "Group successfully deleted.";
     } catch (err) {
         console.log("There was a failure calling deleteGroup");
         console.error(err);
+        return "Failed to delete group."; 
     }
 }
 
