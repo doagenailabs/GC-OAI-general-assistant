cc8e015cf7d7e8ea97c6b6e8017b0fc7c31175a7
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-13 20:31:19 +0100

    Create OAIAssistant.js

---
+++ OAIAssistant.js
@@ -0,0 +1,44 @@
+// openaiAssistant.js
+
+async function sendMessageToOpenAI(message) {
+    try {
+
+        const config = await getConfig();
+
+        const requestBody = {
+            model: "text-davinci-003",
+            prompt: message,
+            max_tokens: 150
+        };
+
+
+        const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${config.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        const data = await response.json();
+        return data.choices[0].text;
+    } catch (error) {
+        console.error('Error sending message to OpenAI:', error);
+        return "Sorry, I couldn't process your request.";
+    }
+}
+
+document.getElementById('openai-send-button').addEventListener('click', async () => {
+    const inputElement = document.getElementById('openai-chat-input');
+    const userMessage = inputElement.value;
+    inputElement.value = '';
+
+    // Display user message in the chat window
+    // ...
+
+    const openaiResponse = await sendMessageToOpenAI(userMessage);
+
+    // Display OpenAI's response in the chat window
+    // ...
+});
