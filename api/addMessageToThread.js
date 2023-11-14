2e8f03401f152f488b31a278dafaaae81fcd6b06
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-14 19:21:41 +0100

    Update addMessageToThread.js

--- api/addMessageToThread.js
+++ api/addMessageToThread.js
@@ -4,16 +4,18 @@ const openai = new OpenAI({
     apiKey: process.env.OAIApiKey
 });
 
-async function addMessageToThread(threadId, messageContent) {
+async function addMessageToThread(req, res) {
+    const { threadId, messageContent } = req.body;
+
     try {
         const message = await openai.beta.threads.messages.create(threadId, {
             role: "user",
             content: messageContent
         });
-        return message;
+        res.json(message);
     } catch (error) {
-        console.error(`Error in addMessageToThread:`, error.toString());
-        throw error;
+        console.error(`Error in addMessageToThread:`, error);
+        res.status(500).json({ error: error.message });
     }
 }
 
