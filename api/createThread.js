e6ce271dd07ec6d09ac978ae2cd628081a72e224
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-14 18:53:20 +0100

    Update createThread.js

--- api/createThread.js
+++ api/createThread.js
@@ -5,11 +5,24 @@ const openai = new OpenAI({
 });
 
 async function createThread() {
+    console.log('Creating thread...');
+
+    console.log(`API Key Length: ${process.env.OAIApiKey ? process.env.OAIApiKey.length : 'undefined'}`);
+
     try {
         const thread = await openai.beta.threads.create();
+        console.log('Thread created:', thread);
         return thread;
     } catch (error) {
-        console.error(`Error in createThread:`, error.toString());
+        console.error(`Error in createThread:`, error);
+
+        if (error.response) {
+            console.error('Response:', error.response);
+            console.error('Status:', error.response.status);
+            console.error('Headers:', error.response.headers);
+            console.error('Data:', error.response.data);
+        }
+
         throw error;
     }
 }
