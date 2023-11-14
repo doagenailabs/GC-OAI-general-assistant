5d5ed775c1785710abb9c7b2ec2ca90fccdd2bdb
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-14 19:23:23 +0100

    Update runAssistant.js

--- api/runAssistant.js
+++ api/runAssistant.js
@@ -4,16 +4,18 @@ const openai = new OpenAI({
     apiKey: process.env.OAIApiKey
 });
 
-async function runAssistant(threadId, assistantId, instructions) {
+async function runAssistant(req, res) {
+    const { threadId, assistantId, instructions } = req.body;
+
     try {
         const run = await openai.beta.threads.runs.create(threadId, {
             assistant_id: assistantId,
             instructions: instructions
         });
-        return run;
+        res.json(run);
     } catch (error) {
-        console.error(`Error in runAssistant:`, error.toString());
-        throw error;
+        console.error(`Error in runAssistant:`, error);
+        res.status(500).json({ error: error.message });
     }
 }
 
