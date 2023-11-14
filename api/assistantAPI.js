e5c2f7cfd32d7333a4e68caf5e2b810b0d857063
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-14 09:53:55 +0100

    Update assistantAPI.js

--- api/assistantAPI.js
+++ api/assistantAPI.js
@@ -1,44 +1,98 @@
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require('openai');
 
-const apiKey = process.env.OAIApiKey;
-const assistantID = process.env.AssistantID;
-const configuration = new Configuration({ apiKey });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+    apiKey: process.env.OAIApiKey
+});
 
 async function createThread() {
-    const response = await openai.createChat({
-        assistant_id: assistantID
-    });
-    return response.data;
+    const emptyThread = await openai.beta.threads.create();
+    return emptyThread;
 }
 
 async function sendMessage(threadId, message) {
-    const response = await openai.createChatMessage({
-        chat_id: threadId,
+    const threadMessages = await openai.beta.threads.messages.create(threadId, {
         role: "user",
         content: message
     });
-    return response.data;
+    return threadMessages;
 }
 
-async function runAssistant(threadId) {
-    const response = await openai.createChatRun({
-        chat_id: threadId
-    });
-    return response.data;
+async function runAssistant(threadId, assistantId) {
+    const run = await openai.beta.threads.runs.create(threadId, { assistant_id: assistantId });
+    return run;
 }
 
 async function getAssistantResponse(threadId, runId) {
-    const response = await openai.getChatRun({
-        chat_id: threadId,
-        run_id: runId
+    const run = await openai.beta.threads.runs.retrieve(threadId, runId);
+    return run;
+}
+
+async function retrieveThread(threadId) {
+    const myThread = await openai.beta.threads.retrieve(threadId);
+    return myThread;
+}
+
+async function modifyThread(threadId, metadata) {
+    const updatedThread = await openai.beta.threads.update(threadId, { metadata });
+    return updatedThread;
+}
+
+async function deleteThread(threadId) {
+    const response = await openai.beta.threads.del(threadId);
+    return response;
+}
+
+async function retrieveMessage(threadId, messageId) {
+    const message = await openai.beta.threads.messages.retrieve(threadId, messageId);
+    return message;
+}
+
+async function modifyMessage(threadId, messageId, metadata) {
+    const updatedMessage = await openai.beta.threads.messages.update(threadId, messageId, { metadata });
+    return updatedMessage;
+}
+
+async function listMessages(threadId) {
+    const threadMessages = await openai.beta.threads.messages.list(threadId);
+    return threadMessages.data;
+}
+
+async function listRuns(threadId) {
+    const runs = await openai.beta.threads.runs.list(threadId);
+    return runs;
+}
+
+async function modifyRun(threadId, runId, metadata) {
+    const updatedRun = await openai.beta.threads.runs.update(threadId, runId, { metadata });
+    return updatedRun;
+}
+
+async function submitToolOutputsToRun(threadId, runId, toolOutputs) {
+    const run = await openai.beta.threads.runs.submitToolOutputs(threadId, runId, { tool_outputs: toolOutputs });
+    return run;
+}
+
+async function createThreadAndRun(assistantId, messages) {
+    const run = await openai.beta.threads.createAndRun({
+        assistant_id: assistantId,
+        thread: { messages }
     });
-    return response.data;
+    return run;
 }
 
 module.exports = {
     createThread,
     sendMessage,
     runAssistant,
-    getAssistantResponse
+    getAssistantResponse,
+    retrieveThread,
+    modifyThread,
+    deleteThread,
+    retrieveMessage,
+    modifyMessage,
+    listMessages,
+    listRuns,
+    modifyRun,
+    submitToolOutputsToRun,
+    createThreadAndRun
 };
