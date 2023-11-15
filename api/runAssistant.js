91b28b1611c96be8f4a235e93bf91ba99032bb60
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-15 07:47:47 +0100

    Update runAssistant.js

--- api/runAssistant.js
+++ api/runAssistant.js
@@ -1,22 +1,21 @@
 const OpenAI = require('openai');
 
+// Retrieve the API key and Assistant ID from environment variables
 const apiKey = process.env.OAI_API_KEY;
 const assistantId = process.env.OPENAI_ASSISTANT_ID;
 
 const openai = new OpenAI({
-    apiKey: apiKey,
-    assistantId: assistantId
+    apiKey: apiKey
 });
 
 async function runAssistant(req, res) {
     // Log request body
     console.log('Request body:', req.body);
 
-    const { threadId, assistantId, instructions } = req.body;
+    const { threadId, instructions } = req.body;
 
     // Log extracted variables
     console.log('Thread ID:', threadId);
-    console.log('Assistant ID:', assistantId);
     console.log('Instructions:', instructions);
 
     try {
