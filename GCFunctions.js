22b7ab5df66b7cfea64ea74d1b04cae10942f4ee
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 17:10:39 +0100

    Create GCFunctions.js

---
+++ GCFunctions.js
@@ -0,0 +1,20 @@
+async function deleteGroup(groupId) {
+    if (!window.platformClient) {
+        console.error("Platform client is not available");
+        return "Platform client not available.";
+    }
+
+    let apiInstance = new window.platformClient.GroupsApi();
+
+    try {
+        await apiInstance.deleteGroup(groupId);
+        console.log("Group deleted successfully.");
+        return "Group deleted successfully.";
+    } catch (error) {
+        console.error('Error in deleteGroup:', error);
+        return "Failed to delete group.";
+    }
+}
+
+// Attach the function to the global window object
+window.deleteGroup = deleteGroup;
