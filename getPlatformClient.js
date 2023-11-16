99ddb5aff152b768151d9d71ebf36d670821d56f
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 14:49:02 +0100

    Delete getPlatformClient.js

--- getPlatformClient.js
+++
@@ -1,8 +0,0 @@
-function getPlatformClient() {
-    const platformClient = window.platformClient;
-    if (!platformClient) {
-        console.error('platformClient is not initialized.');
-        return null;
-    }
-    return platformClient;
-}
