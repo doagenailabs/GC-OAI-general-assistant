65e835f995867b4f5a75df3ccd7e4797163fb8b6
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 16:36:35 +0100

    Update submitToolOutputs.js

--- api/submitToolOutputs.js
+++ api/submitToolOutputs.js
@@ -10,17 +10,31 @@ async function submitToolOutputs(req, res) {
     console.log('Submitting tool outputs for Thread ID:', threadId, 'and Run ID:', runId);
 
     try {
-        // Here, you would typically process or log the tool outputs.
-        // For the current use case, we are just logging the outputs.
-        console.log('Tool outputs:', tool_outputs);
+        // Process the tool outputs and construct a message to send back to the assistant
+        let responseMessages = [];
+        for (const output of tool_outputs) {
+            const outputMessage = constructOutputMessage(output); // Function to construct a message from the tool output
+            const message = await openai.beta.threads.messages.create(threadId, {
+                role: "user",
+                content: outputMessage
+            });
+            responseMessages.push(message);
+        }
 
-        // Since this is just an acknowledgement endpoint and doesn't need to send data back to OpenAI,
-        // we simply return a success response.
-        res.json({ message: 'Tool outputs submitted successfully', threadId: threadId, runId: runId });
+        // Respond with the messages that were sent to the assistant
+        res.json({ message: 'Tool outputs submitted and responses sent to assistant', threadId: threadId, runId: runId, responses: responseMessages });
     } catch (error) {
         console.error(`Error in submitToolOutputs:`, error);
         res.status(500).json({ error: error.message });
     }
+}
+
+// Helper function to construct a message based on tool output
+function constructOutputMessage(output) {
+    // Depending on the output structure, create a message
+    // This is a placeholder; customize this based on how your tool outputs are structured
+    const message = `Function ${output.tool_call_id} completed with result: ${output.output}`;
+    return message;
 }
 
 module.exports = submitToolOutputs;
