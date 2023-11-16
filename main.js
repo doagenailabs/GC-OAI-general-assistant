80bbf3783850d26da959c798ed0b431092b5158e
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 21:27:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 async function loadExistingThread() {
+    console.log('Loading existing thread...');
     const threadId = localStorage.getItem('currentThreadId');
+    console.log('Current thread ID:', threadId);
+
     if (threadId) {
         try {
+            console.log('Fetching messages for thread:', threadId);
             const response = await fetch(`/api/displayAssistantResponse?threadId=${threadId}`);
             const messages = await response.json();
 
-            const displayedMessageIds = new Set(); // Set to track displayed message IDs
+            const displayedMessageIds = new Set();
+            console.log('Sorting and displaying messages...');
             messages.data.sort((a, b) => a.created_at - b.created_at).forEach(message => {
-                if (!displayedMessageIds.has(message.id)) { // Check if the message ID is already displayed
+                if (!displayedMessageIds.has(message.id)) {
                     displayedMessageIds.add(message.id);
+                    console.log('Displaying message:', message.id);
                     const isUserMessage = message.role === "user";
                     message.content.forEach(contentPart => {
                         if (contentPart.type === "text") {
                             displayMessage(contentPart.text.value, isUserMessage);
                         }
                     });
+                } else {
+                    console.log('Skipping already displayed message:', message.id);
                 }
             });
         } catch (error) {
@@ -24,26 +32,29 @@ async function loadExistingThread() {
 }
 
 async function handleUserInput(userMessage) {
+    console.log('Handling user input:', userMessage);
     showLoadingIcon(true);
 
     try {
         let threadId = localStorage.getItem('currentThreadId');
+        console.log('Current thread ID:', threadId);
 
         if (!threadId) {
+            console.log('Creating new thread...');
             const thread = await fetch('/api/createThread').then(response => response.json());
             threadId = thread.id;
             localStorage.setItem('currentThreadId', threadId);
             console.log('New Thread ID:', threadId);
-        } else {
-            console.log('Using existing Thread ID:', threadId);
         }
 
+        console.log('Adding message to thread:', threadId);
         await fetch('/api/addMessageToThread', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ threadId: threadId, messageContent: userMessage })
         });
 
+        console.log('Running assistant...');
         let run = await fetch('/api/runAssistant', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -52,10 +63,12 @@ async function handleUserInput(userMessage) {
 
         let assistantResponse;
         do {
+            console.log('Checking run status:', run.id);
             assistantResponse = await fetch(`/api/checkRunStatus?threadId=${threadId}&runId=${run.id}`)
                 .then(response => response.json());
 
             if (assistantResponse.status === 'requires_action') {
+                console.log('Handling tool calls...');
                 await handleToolCalls(assistantResponse.required_action.submit_tool_outputs.tool_calls, threadId, run.id);
                 run = await fetch(`/api/checkRunStatus?threadId=${threadId}&runId=${run.id}`)
                     .then(response => response.json());
@@ -64,12 +77,15 @@ async function handleUserInput(userMessage) {
             await new Promise(resolve => setTimeout(resolve, 1000));
         } while (assistantResponse.status !== 'completed');
 
+        console.log('Fetching display assistant response...');
         const messages = await fetch(`/api/displayAssistantResponse?threadId=${threadId}`)
             .then(response => response.json());
 
-        messages.data.filter(message => message.role === "assistant" && (!window.lastAssistantMessageId || message.id > lastAssistantMessageId))
+        console.log('Filtering and displaying new assistant messages...');
+        messages.data.filter(message => message.role === "assistant" && (!window.lastAssistantMessageId || message.id > window.lastAssistantMessageId))
             .forEach(message => {
                 window.lastAssistantMessageId = message.id;
+                console.log('Displaying message:', message.id);
                 message.content.forEach(contentPart => {
                     if (contentPart.type === "text") {
                         displayMessage(contentPart.text.value, false);
