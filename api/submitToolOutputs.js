5a309de91b03041fab5dbdce27daea6e4064482c
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 17:44:14 +0100

    Update submitToolOutputs.js

--- api/submitToolOutputs.js
+++ api/submitToolOutputs.js
@@ -1,33 +1,43 @@
 const OpenAI = require('openai');
 
 const apiKey = process.env.OAI_API_KEY;
-const openai = new OpenAI({
-    apiKey: apiKey
-});
+const openai = new OpenAI({ apiKey: apiKey });
 
 async function submitToolOutputs(req, res) {
     console.log('Received request in submitToolOutputs:', req.body);
 
-    const { threadId, runId, tool_outputs } = req.body;
+    const { threadId, tool_outputs } = req.body;
+    console.log('Thread ID:', threadId, 'Tool Outputs:', tool_outputs);
 
-    console.log('Thread ID:', threadId, 'Run ID:', runId, 'Tool Outputs:', tool_outputs);
-
-    if (!threadId || !runId) {
-        console.error('Thread ID or Run ID is undefined.');
-        return res.status(400).json({ error: 'Thread ID or Run ID is undefined.' });
+    if (!threadId) {
+        console.error('Thread ID is undefined.');
+        return res.status(400).json({ error: 'Thread ID is undefined.' });
     }
 
     try {
-        let toolResponseMessages = tool_outputs.map(output => ({
-            role: "tool",
-            name: output.tool_call_id, 
-            content: constructOutputMessage(output),
-            tool_call_id: output.tool_call_id // Linking to the original tool call
-        }));
+        // Retrieve the existing thread
+        const threadResponse = await openai.beta.threads.retrieve(threadId);
+        let messages = threadResponse.data.messages;
 
+        // Integrate tool outputs into the conversation
+        for (const output of tool_outputs) {
+            // Find the position to insert the tool response
+            let insertPosition = messages.findIndex(msg => msg.id === output.tool_call_id);
+            if (insertPosition !== -1) {
+                insertPosition++; // Insert after the tool call
+                const toolResponseMessage = {
+                    role: "tool",
+                    content: constructOutputMessage(output),
+                    tool_call_id: output.tool_call_id
+                };
+                messages.splice(insertPosition, 0, toolResponseMessage); // Insert the tool response
+            }
+        }
+
+        // Send the updated conversation to the model
         const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo-1106",
-            messages: toolResponseMessages
+            messages: messages
         });
 
         res.json({ message: 'Function outputs submitted and response received from assistant', response: response.data });
@@ -38,8 +48,7 @@ async function submitToolOutputs(req, res) {
 }
 
 function constructOutputMessage(output) {
-    // Construct a message based on tool output
-    const message = JSON.stringify({ result: output.output }); 
+    const message = JSON.stringify({ result: output.output });
     return message;
 }
 
