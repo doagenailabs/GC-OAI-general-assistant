358468b54d25716b97e639251d1431e6e3c4b8cf
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-16 20:20:48 +0100

    Update toolCallHandler.js

--- toolCallHandler.js
+++ toolCallHandler.js
@@ -1,8 +1,7 @@
 async function handleToolCalls(toolCalls, threadId, runId) {
     for (const call of toolCalls) {
-        // Check if this tool call has already been handled
         if (localStorage.getItem(call.id) === 'completed') {
-            continue; // Skip this call as it has already been handled
+            continue;
         }
 
         let resultMessage;
@@ -10,21 +9,19 @@ async function handleToolCalls(toolCalls, threadId, runId) {
             case 'deleteGroup':
                 const groupId = JSON.parse(call.function.arguments).groupId;
                 resultMessage = await window.deleteGroup(groupId);
-                displayMessage(resultMessage, false); // false for assistant message
                 break;
             // Add cases for other functions as needed
         }
 
         const outputs = [{ tool_call_id: call.id, output: resultMessage || "Completed" }];
-        
-        // Send threadId and runId in the request body
+
+        // Send the result message, threadId, and runId in the request body
         await fetch(`/api/submitToolOutputs`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ threadId: threadId, runId: runId, tool_outputs: outputs })
+            body: JSON.stringify({ threadId: threadId, runId: runId, tool_outputs: outputs, resultMessage: resultMessage })
         });
 
-        // Mark this tool call as completed to avoid reprocessing
         localStorage.setItem(call.id, 'completed');
     }
 }
