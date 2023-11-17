6a80d4c2259a2f2354686397c4e54e7009423f3f
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-17 09:35:25 +0100

    Update uploadFile.js

--- api/uploadFile.js
+++ api/uploadFile.js
@@ -1,3 +1,4 @@
+const multiparty = require('multiparty');
 const fs = require('fs');
 const path = require('path');
 const OpenAI = require('openai');
@@ -5,36 +6,47 @@ const OpenAI = require('openai');
 const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey: apiKey });
 
-async function uploadFile(req, res) {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        console.error('No files were uploaded in the request');
-        return res.status(400).send('No files were uploaded.');
-    }
+module.exports = (req, res) => {
+    if (req.method === "POST") {
+        let form = new multiparty.Form();
 
-    let uploadedFile = req.files.file;
-    console.log('Received file for upload:', uploadedFile.name);
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                console.error('Error parsing form:', err);
+                res.status(500).send('Error parsing form data');
+                return;
+            }
 
-    let tempPath = path.join(__dirname, 'uploads', uploadedFile.name);
-    
-    // Save file temporarily
-    await uploadedFile.mv(tempPath);
+            if (!files || !files.file) {
+                console.error('No files were uploaded in the request');
+                res.status(400).send('No files were uploaded.');
+                return;
+            }
 
-    try {
-        console.log('Uploading file to OpenAI:', uploadedFile.name);
-        const file = await openai.files.create({
-            file: fs.createReadStream(tempPath),
-            purpose: "assistants",
+            let uploadedFile = files.file[0];
+            console.log('Received file for upload:', uploadedFile.originalFilename);
+
+            try {
+                console.log('Uploading file to OpenAI:', uploadedFile.originalFilename);
+                const file = await openai.files.create({
+                    file: fs.createReadStream(uploadedFile.path),
+                    purpose: "assistants",
+                });
+
+                console.log('File uploaded to OpenAI successfully:', uploadedFile.originalFilename);
+                // Delete the temporary file
+                fs.unlinkSync(uploadedFile.path);
+
+                res.json({ file_id: file.id });
+            } catch (error) {
+                console.error(`Error in uploadFile while uploading to OpenAI:`, error);
+                fs.unlinkSync(uploadedFile.path); // Ensure to delete temporary file in case of error
+                res.status(500).json({ error: error.message });
+            }
         });
-
-        console.log('File uploaded to OpenAI successfully:', uploadedFile.name);
-        // Delete the temporary file
-        fs.unlinkSync(tempPath);
-
-        res.json({ file_id: file.id });
-    } catch (error) {
-        console.error(`Error in uploadFile while uploading to OpenAI:`, error);
-        res.status(500).json({ error: error.message });
+    } else {
+        res.status(405).send("Method not allowed");
     }
-}
+};
 
 module.exports = uploadFile;
