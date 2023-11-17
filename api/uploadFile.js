1b5e1c06d5d9805adb4cb8129889bf61a3b60b26
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-17 08:46:58 +0100

    Update uploadFile.js

--- api/uploadFile.js
+++ api/uploadFile.js
@@ -1,32 +1,42 @@
 const OpenAI = require('openai');
 const fs = require('fs');
+const multer = require('multer');
 
 const apiKey = process.env.OPENAI_API_KEY;
+const upload = multer({ dest: 'uploads/' });
 
 const openai = new OpenAI({
     apiKey: apiKey
 });
 
-async function uploadFile(req, res) {
-    try {
-        // Ensure the file is attached in the request and available under req.file
-        // This requires file handling middleware like multer to be configured correctly
+module.exports = async (req, res) => {
+    upload.single('file')(req, res, async (err) => {
+        if (err) {
+            res.status(500).send(`Multer error: ${err}`);
+            return;
+        }
+
         if (!req.file) {
             res.status(400).send('No file uploaded.');
             return;
         }
 
-        const fileStream = fs.createReadStream(req.file.path);
-        const fileResponse = await openai.files.create({
-            file: fileStream,
-            purpose: "assistants"
-        });
+        try {
+            const fileStream = fs.createReadStream(req.file.path);
+            const fileResponse = await openai.files.create({
+                file: fileStream,
+                purpose: "assistants"
+            });
 
-        res.json({ fileId: fileResponse.id });
-    } catch (error) {
-        console.error('Error in uploadFile:', error);
-        res.status(500).json({ error: error.message });
-    }
-}
+            // Clean up the uploaded file
+            fs.unlink(req.file.path, (err) => {
+                if (err) console.error(`Error deleting file: ${err}`);
+            });
 
-module.exports = uploadFile;
+            res.json({ fileId: fileResponse.id });
+        } catch (error) {
+            console.error('Error in uploadFile:', error);
+            res.status(500).json({ error: error.message });
+        }
+    });
+};
