860dbd5e85b24d11c8a08b8c292f716d2b1d6753
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-17 09:26:02 +0100

    Update uploadFile.js

--- api/uploadFile.js
+++ api/uploadFile.js
@@ -7,27 +7,32 @@ const openai = new OpenAI({ apiKey: apiKey });
 
 async function uploadFile(req, res) {
     if (!req.files || Object.keys(req.files).length === 0) {
+        console.error('No files were uploaded in the request');
         return res.status(400).send('No files were uploaded.');
     }
 
     let uploadedFile = req.files.file;
+    console.log('Received file for upload:', uploadedFile.name);
+
     let tempPath = path.join(__dirname, 'uploads', uploadedFile.name);
     
     // Save file temporarily
     await uploadedFile.mv(tempPath);
 
     try {
+        console.log('Uploading file to OpenAI:', uploadedFile.name);
         const file = await openai.files.create({
             file: fs.createReadStream(tempPath),
             purpose: "assistants",
         });
 
+        console.log('File uploaded to OpenAI successfully:', uploadedFile.name);
         // Delete the temporary file
         fs.unlinkSync(tempPath);
 
         res.json({ file_id: file.id });
     } catch (error) {
-        console.error(`Error in uploadFile:`, error);
+        console.error(`Error in uploadFile while uploading to OpenAI:`, error);
         res.status(500).json({ error: error.message });
     }
 }
