70af6f18d6b505e3774c81deda14f897815a6d05
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-23 21:00:18 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -2,6 +2,6 @@ module.exports = (req, res) => {
     res.json({
         GCclientId: process.env.GC_OAUTH_CLIENT_ID
         //apiKey: process.env.OPENAI_API_KEY,
-        //assistantId: process.env.OPENAI_ASSISTANT_ID
+        //assistantId: assistantId
     });
 };
