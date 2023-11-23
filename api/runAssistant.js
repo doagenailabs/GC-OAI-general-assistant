2449f8c8b102a8474f29e9b4e7f70c825ba165cb
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-23 21:51:13 +0100

    Update runAssistant.js

--- api/runAssistant.js
+++ api/runAssistant.js
@@ -1,14 +1,35 @@
 const OpenAI = require('openai');
 
+// Importing environment variables for different OpenAI assistants
 const apiKey = process.env.OPENAI_API_KEY;
-const assistantId = assistantId;
+const groupAssistantId = process.env.OPENAI_GROUP_ASSISTANT_ID;
+const queueAssistantId = process.env.OPENAI_QUEUE_ASSISTANT_ID;
+const userAssistantId = process.env.OPENAI_USER_ASSISTANT_ID;
 
 const openai = new OpenAI({
     apiKey: apiKey
 });
 
 async function runAssistant(req, res) {
-    const { threadId, instructions } = req.body;
+    const { threadId, instructions, assistantType } = req.body;
+
+    // Determine the assistant ID based on the type
+    let assistantId;
+    switch (assistantType) {
+        case 'groups':
+            assistantId = groupAssistantId;
+            break;
+        case 'queues':
+            assistantId = queueAssistantId;
+            break;
+        case 'users':
+            assistantId = userAssistantId;
+            break;
+        default:
+            // Default assistant ID if none is specified
+            assistantId = groupAssistantId;
+            break;
+    }
 
     try {
         const run = await openai.beta.threads.runs.create(threadId, {
