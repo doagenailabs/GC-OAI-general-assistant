ed992b463a6fc7815c45f617966740a9de1a3a5c
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-24 13:56:11 +0100

    Update startGCSDKs.js

--- startGCSDKs.js
+++ startGCSDKs.js
@@ -6,7 +6,7 @@ function startGCSDKs() {
         const qParamGcHostOrigin = 'gcHostOrigin';
         const qParamEnvironment = 'gcTargetEnv';
         let language = '';  
-        let redirectUri = 'https://gcloudassistant.vercel.app';
+        let redirectUri = 'https://gencloudassistant.vercel.app';
         let userDetails = null;
         let gcHostOrigin = '';    
         assignConfiguration();
