0b092dd0d77d2e599f9da70791c8adf9e0653674
doagenailabs <140493630+doagenailabs@users.noreply.github.com>
2023-11-24 09:31:58 +0100

    Update GCFunctions.js

--- GCFunctions.js
+++ GCFunctions.js
@@ -18,3 +18,63 @@ async function deleteGroup(groupId) {
 
 // Attach the function to the global window object
 window.deleteGroup = deleteGroup;
+
+async function getQueueDetails(queueId) {
+    if (!window.platformClient) {
+        console.error("Platform client is not available");
+        return "Error accessing platform client";
+    }
+
+    let apiInstance = new window.platformClient.RoutingApi();
+
+    try {
+        let queueDetails = await apiInstance.getRoutingQueue(queueId);
+        console.log("Queue details retrieved successfully.");
+        return JSON.stringify(queueDetails);
+    } catch (error) {
+        console.error('Error in getQueueDetails:', error);
+        return `Error retrieving queue details: ${error.message}`;
+    }
+}
+
+window.getQueueDetails = getQueueDetails;
+
+async function getEstimatedWaitTime(queueId, mediaType) {
+    if (!window.platformClient) {
+        console.error("Platform client is not available");
+        return "Error accessing platform client";
+    }
+
+    let apiInstance = new window.platformClient.RoutingApi();
+
+    try {
+        let waitTime = await apiInstance.getRoutingQueueMediatypesEstimatedwaittime(queueId, mediaType);
+        console.log("Estimated wait time retrieved successfully.");
+        return JSON.stringify(waitTime);
+    } catch (error) {
+        console.error('Error in getEstimatedWaitTime:', error);
+        return `Error retrieving estimated wait time: ${error.message}`;
+    }
+}
+
+window.getEstimatedWaitTime = getEstimatedWaitTime;
+
+async function getQueueMembers(queueId) {
+    if (!window.platformClient) {
+        console.error("Platform client is not available");
+        return "Error accessing platform client";
+    }
+
+    let apiInstance = new window.platformClient.RoutingApi();
+
+    try {
+        let members = await apiInstance.getRoutingQueueMembers(queueId);
+        console.log("Queue members retrieved successfully.");
+        return JSON.stringify(members);
+    } catch (error) {
+        console.error('Error in getQueueMembers:', error);
+        return `Error retrieving queue members: ${error.message}`;
+    }
+}
+
+window.getQueueMembers = getQueueMembers;
